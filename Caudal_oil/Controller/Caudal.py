593a0e50590f7aea0de36feaf900cacba5dd43b2
Cristina Viñanzaca <cavinanz@espol.edu>
2023-07-04 10:50:07 -0500

    cambios en funcion

--- Caudal_oil/Controller/Caudal.py
+++ Caudal_oil/Controller/Caudal.py
@@ -1,15 +1,31 @@
 import xlwings as xw
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
+
+from Caudal_oil.Model.Funcion import caudal_oil
+
+HOJA = "Hoja1"
+QMAX = "Qmax"
+PWF = "Pwf"
+PR = "Pr"
+QO = "Qo"
 def main():
     wb = xw.Book.caller()
-    HOJA = wb["Hoja1"]
-    QMAX = HOJA["Qmax"]
-    PWF = HOJA["Pwf"]
-    PR = HOJA["Pr"]
-    QO = HOJA["Qo"]
+    hoja = wb.sheets[HOJA]
+    qmax = hoja[QMAX]
+    pr = hoja[PR]
+    pwf = hoja[PWF].value
+
+    hoja[QO].value = caudal_oil(qmax,pwf,pr)
 
 
+    # codigo para la gráfica
+
+    fig, ax = plt.subplots()
+    ax.plot(HOJA[PWF], HOJA[QO])
+    hoja.pictures.add(fig, name="Pwf vs Qo", update=True, left=hoja.range("B16").left, pop=hoja.range("B16").pop)
 
 if __name__ == "__main__":
     xw.Book("Caudal.xlsm").set_mock_caller()
